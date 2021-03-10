4c827bf998ea3c7c8631c9c81729b7d29c947c9c
TadashiMatsumoto <tad@viva-life.biz>
2021-03-10 22:03:10 +0900

    Initial Commit

---
+++ Shared/SampleSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftApp.swift
+//  Shared
+//
+//  Created by Tadashi Matsumoto on 2021/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
